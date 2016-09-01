5288fe42e03cb647292d0e7f0ba23fd13f3bf2fe
yuluo_lp <btc022003@gmail.com>
2016-09-01 17:01:55 +0800

    解决跨域请求无法发送put、delete的问题
    和客户端代码无关,主要是在服务器端对接口代码做了调整

--- src/views/About.vue
+++ src/views/About.vue
@@ -23,20 +23,37 @@ export default {
   },
   computed: {},
   ready() {
+
+    /**
+     *
+     * 此问题以解决
+     * 原因:
+     * W3C规范这样要求在跨域请求中，分为简单请求（get和部分post，post时content-type属于application/x-www-form-urlencoded，multipart/form-data，text/plain中的一种）和复杂请求。而复杂请求发出之前，就会出现一次options请求。
+     *
+     * 解决办法：在服务器端多做一次options方式提交的响应处理
+     */
+
+    // var resource = this.$resource('http://localhost:3000/home{/id}');
+    // resource.delete({id: 1}).then((response) => {
+    //       // success callback
+    //   }, (response) => {
+    //       // error callback
+    //   });
+
     // console.log(this.$http);
     // get
-    this.$http.get('http://lixuanqi.com/api/v1/address/get_data.json').then((res)=>{
-      console.dir(res.data);
-    }).catch((err)=>{
-      console.dir(err);
-    });
-
-    // post
-    this.$http.post('http://localhost:3000/home',{name:"tom"}).then((res)=>{
-      console.dir(res);
-    }).catch((err)=>{
-      console.dir(err);
-    });
+    // this.$http.get('http://lixuanqi.com/api/v1/address/get_data.json').then((res)=>{
+    //   console.dir(res.data);
+    // }).catch((err)=>{
+    //   console.dir(err);
+    // });
+    //
+    // // post
+    // this.$http.post('http://localhost:3000/home',{name:"tom"}).then((res)=>{
+    //   console.dir(res);
+    // }).catch((err)=>{
+    //   console.dir(err);
+    // });
 
     /**
      * get post可用
@@ -47,6 +64,46 @@ export default {
     // },(err)=>{
     //   console.log(err);
     // });
+    //
+
+    /**
+     * 使用jQuery测试ajax请求
+     * 依然无法解决put delete的问题
+     */
+    // $.ajax({
+    //   type:"get",
+    //   url:"http://lixuanqi.com/api/v1/address/get_data.json",
+    //   success:function(res){
+    //     console.log(res);
+    //   },
+    //   erroe:function(err){
+    //     console.log(err);
+    //   }
+    // })
+    //
+    // $.ajax({
+    //   type:"post",
+    //   url:"http://localhost:3000/home",
+    //   data:{name:"Jerry"},
+    //   success:function(res){
+    //     console.log(res);
+    //   },
+    //   erroe:function(err){
+    //     console.log(err);
+    //   }
+    // })
+    //
+    // $.ajax({
+    //   type:"put",
+    //   url:"http://localhost:3000/home/1",
+    //   data:{name:"Jerry"},
+    //   success:function(res){
+    //     console.log(res);
+    //   },
+    //   erroe:function(err){
+    //     console.log(err);
+    //   }
+    // })
 
     console.log('ready');
   },
